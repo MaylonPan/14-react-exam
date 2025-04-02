66ca08119128cbbcb83d8f9b737adac58368ae35
Kriangkai <maylonpan@gmail.com>
2025-04-02 14:08:49 +0700

    First Push

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-export default function App() {
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-  return (
-    <div className="h-screen flex justify-center bg-blue-950 ">
-    <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
-      <h1 className="w-full p-6 bg-amber-100 font-extrabold">
-        React App Starter 💖
-      </h1>
-      <section className="w-full p-5 bg-amber-100 flex">
-        <ul className="list-inside list-disc flex-1">
-          <span className="font-semibold">Tech Stack:</span>
-          <li>Vite</li>
-          <li>React</li>
-          <li>JavaScript</li>
-          <li>Tailwind</li>
-        </ul>
-      </section>
-    </div>
-  </div>
-  )
+import Home from "./pages/Home";
+// import UserHomeSession from "./pages/UserHomeSession";
+// import AdminHomeSession from "./pages/AdminHomeSession";
+// import OwnerPage from "./pages/OwnerPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {path: "/", element: <Home />},
+      // {path: "owner", element: <OwnerPage />},
+      // {path: "admin", element: <AdminHomeSession />},
+      // {path: "user", element: <UserHomeSession />},
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
 }
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,56 @@
+import Axios from "axios";
+import { Outlet, Link, useNavigate } from "react-router-dom";
+
+const Home = () => {
+
+  const navigate = useNavigate();
+
+  const handleUser = () => {
+    navigate("/user");
+  };
+  const handleAdmin = () => {
+    navigate("/admin")
+  };
+
+    return (
+    <div className="h-screen flex justify-center bg-[#27445D] ">
+        <section className="relative">
+        <nav className="flex flex-row justify-end p-5 pr-30 border-b-1 fixed top-0 right-0 left-0">
+          <ul className="flex gap-15 font-bold text-white">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/">Owner</Link>
+              </li>
+          </ul>
+        </nav>
+      </section>
+
+
+      <main className="flex w-full justify-center pt-[80px]">
+        <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%]">
+          <h1 className="w-full p-6 text-5xl text-white font-extrabold text-center">
+            Generation Thailand React - Assessment
+          </h1>
+          <section className="w-full p-2 px-20 flex justify-center gap-50">
+            <button
+            onClick={handleUser}
+            className="bg-white shadow-2xl p-4 font-bold rounded-xl hover:bg-[#008170] hover:scale-105 duration-300 hover:text-white">User Home Sector
+            </button>
+
+            <button
+            onClick={handleAdmin}
+            className="bg-white shadow-2xl p-4 font-bold rounded-xl hover:bg-[#008170] hover:scale-105 duration-300 hover:text-white">Admin Home Sector</button>
+          </section>
+          <section>
+
+          </section>
+        </div>
+      </main>
+
+    </div>
+    )
+}
+
+export default Home;
